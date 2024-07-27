94feb574177a6d085992bb0dd4c094d8cf5e00f9
Ichsan Nuur Imansyah <48359338+ichsanni@users.noreply.github.com>
2024-07-28 05:45:11 +0700

    fixed hydration error

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../public/css/dashboard.css'
 import '../public/css/_custom.scss';
-import '../lib/fontawesome'; 
+import {useEffect, useState} from "react";
 
 function MyApp({ Component, pageProps }) {
+  const [isFirst, setFirst] = useState(false);
+  useEffect(() => {
+    if(!isFirst){
+      setFirst(true);
+      return
+    };
+    (async () => await import('../lib/fontawesome'))();
+  }, [isFirst])
   return <Component {...pageProps} />;
 }
 
